e842ab410f3dd8517476582ec7774e32d2a1acaa
Amod Shinde <amodshinde1995@gmail.com>
2021-01-20 14:43:29 +0530

    Added mock data

---
+++ db.js
@@ -0,0 +1,11 @@
+{
+  "data": [
+          { icon: `😀`, text: "Page 1" },
+          { icon: `😉`, text: "Page 2" },
+          { icon: `😎`, text: "Page 3" },
+          { icon: `🤔`, text: "Page 4" },
+          { icon: `😛`, text: "Page 5" },
+          { icon: `😺️`, text: "Page 6" },
+          { icon: `⚙`, text: "Page 7" },
+        ]
+}
